c04725433da6495218a19667298e3b92a327f360
jopdop30 <45281927+jopdop30@users.noreply.github.com>
2019-03-27 20:36:48 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var html_dir = './html/';
 
-app.use(express.static(path.join(__dirname, 'html')));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/login', function(req, res) {
-    res.sendfile(html_dir + 'login.html');
+    res.sendfile(__dirname + '/login.html');
 });
 
 io.on('connection', function(socket){
